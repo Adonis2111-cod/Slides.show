2dac5e90b06668f86cf4c1ac3f1a018ba0f4eacb
Adonis Hernandes <61875794+adonis2111@users.noreply.github.com>
2021-04-19 20:00:59 -0300

    Mudei as imagens

--- js/pratica.js
+++ js/pratica.js
@@ -1,43 +1,43 @@
 function mudar_slides(){
-  document.getElementById("img1").src = "img/img1.jpg";
+  document.getElementById("img1").src = "https://cdn.pixabay.com/photo/2018/10/07/10/24/sail-3729599__340.jpg";
 }
 
 function mudar_slides2(){
-    document.getElementById("img1").src = "img/img2.jpg";
+    document.getElementById("img1").src = "https://cdn.pixabay.com/photo/2017/09/17/16/35/boats-2758962__340.jpg";
   }
 
   function mudar_slides3(){
-    document.getElementById("img1").src = "img/img3.jpg";
+    document.getElementById("img1").src = "https://cdn.pixabay.com/photo/2016/08/14/18/27/sailing-boat-1593613__340.jpg";
   }
 
   function mudar_slides4(){
-    document.getElementById("img1").src = "img/Parque1.jpg";
+    document.getElementById("img1").src = "https://cdn.pixabay.com/photo/2012/11/02/13/02/car-63930__340.jpg";
   }
   
   function mudar_slides5(){
-      document.getElementById("img1").src = "img/Parque2.jpg";
+      document.getElementById("img1").src = "https://cdn.pixabay.com/photo/2016/04/01/12/16/car-1300629__340.png";
     }
   
     function mudar_slides6(){
-      document.getElementById("img1").src = "img/Parque3.jpg";
+      document.getElementById("img1").src = "https://cdn.pixabay.com/photo/2017/03/05/15/29/aston-martin-2118857__340.jpg";
     }
 
     function mudar_slides7(){
-      document.getElementById("img1").src = "img/Navio_Pirata.jpg";
+      document.getElementById("img1").src = "https://cdn.pixabay.com/photo/2016/07/05/18/35/aircraft-1499171__340.jpg";
     }
     
     function mudar_slides8(){
-        document.getElementById("img1").src = "img/Navio_Pirata2.jpg";
+        document.getElementById("img1").src = "https://cdn.pixabay.com/photo/2017/09/28/13/38/aircraft-2795557__340.jpg";
       }
     
       function mudar_slides9(){
-        document.getElementById("img1").src = "img/Navio_Pirata3.jpg";
+        document.getElementById("img1").src = "https://cdn.pixabay.com/photo/2018/03/19/20/57/aircraft-3241229__340.jpg";
       }
 
   function mostrar_tela(){
       let tela = document.getElementById("img1");
       tela.style.display="block";
-      tela.src="img/img1.jpg";
+      tela.src="https://cdn.pixabay.com/photo/2018/10/07/10/24/sail-3729599__340.jpg";
       let slide = document.getElementsByClassName("dot");
       slide[0].style.display="block";
       slide[1].style.display="block";
@@ -53,7 +53,7 @@ function mudar_slides2(){
   function mostrar_parque(){
     let parque = document.getElementById("img1");
     parque.style.display="block";
-    parque.src="img/Parque1.jpg";
+    parque.src="https://cdn.pixabay.com/photo/2012/11/02/13/02/car-63930__340.jpg";
     let slide = document.getElementsByClassName("dot");
       slide[0].style.display="none";
       slide[1].style.display="none";
@@ -69,7 +69,7 @@ function mudar_slides2(){
   function mostrar_navio(){
     let parque = document.getElementById("img1");
     parque.style.display="block";
-    parque.src="img/Navio_Pirata.jpg";
+    parque.src="https://cdn.pixabay.com/photo/2016/07/05/18/35/aircraft-1499171__340.jpg";
     let slide = document.getElementsByClassName("dot");
       slide[0].style.display="none";
       slide[1].style.display="none";
